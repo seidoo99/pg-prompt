919a5fcc6590f994cea388dc09e413ad74ce0422
seid zerihun <seidoo99@gmial.com>
2020-05-07 23:25:02 -0400

    second commit

--- album.js
+++ album.js
@@ -8,29 +8,35 @@ const pgp = require('pg-promise')();
 const db = pgp(config);
 var prompt = require('prompt-promise');
 var results = [];
-prompt('album_name: ')
+prompt('artist_id: ')
     .then((val) => {
         results.push(val);
-        return prompt.multiline('artist_id: ');
+        return prompt('release_year: ');
     })
     .then((val) => {
         results.push(val);
-        return prompt.multiline('release_year: ');
+        return prompt('album_name: ');
     })
     .then((val) => {
         results.push(val);
-        console.log('response:', result);
+        console.log('response:', results);
+        insertData();
     })
     .catch(function rejected(err) {
         console.log('error:', err.stack);
         prompt.finish();
+        prompt.done();
     });
 
-var querry = "insert into album(album_name, artist_id, release_year) values($1, $2, $3)";
-db.result(querry, results)
-    .then((result) => {
-        console.log(result);
-    }).catch((e) => {
-        console.error(e);
-    });
-pgp.end();
+
+function insertData() {
+    var album = { artistId: results[0], releaseYear: results[1], albumName: results[2] };
+    var query = `INSERT INTO album (artist_id, release_year, album_name, ) VALUES (${artistId}, ${releaseYear},${albumName})`;
+    db.result(query, album)
+        .then(function(result) {
+            console.log(result);
+            var id = result.rows[0].id;
+            console.log(`Created album with ID ${id}.`);
+            pgp.end();
+        });
+}
